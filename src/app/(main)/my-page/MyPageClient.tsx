1e8c1036a8878767d11ff70a5d0f38c656bd4ac4
yang7493 <yang7493@g.eulji.ac.kr>
2026-09-16 11:52:27 +0900

    Revert "fix(my-page): 일반 회원탈퇴 후 로그인 화면으로 이동하지 않던 문제 수정"
    This reverts commit 067f9fcbcbb55f8ab6d84a23ad2990137a7adab9.

--- src/app/(main)/my-page/MyPageClient.tsx
+++ src/app/(main)/my-page/MyPageClient.tsx
@@ -139,9 +139,6 @@ export function MyPageClient() {
     }
     await logout();
     setWithdrawStep('success');
-    setTimeout(() => {
-      router.push('/login');
-    }, 1400);
   };
 
   const onConfirmBlocked = () => {
